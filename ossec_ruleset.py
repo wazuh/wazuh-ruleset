d9e6001b4d9d337091020056db6bd122f05f8d08
Jesus Linares <jesuslinares90@gmail.com>
2016-06-22 08:58:48 +0000

    JSON help

--- ossec_ruleset.py
+++ ossec_ruleset.py
@@ -1102,7 +1102,7 @@ Backups:
 Additional Params:
 \t-f, --force-update\tForce to update all rules and rootchecks. By default, only it is updated the new/changed rules/rootchecks.
 \t-d, --directory\tUse the ruleset specified at 'directory'. Directory structure should be the same that ossec-rules repository.
-\t-j, --json\nJSON ouput. Use with non-interactive arguments.
+\t-j, --json\nJSON output. Use with non-interactive arguments.
 
 Configuration file syntax using option -A:
 \t# Commented line
