c264881a7c52798b6e4a3efd0e20128fa5f92e55
Adrián Jesús Peña Rodríguez <adrianjpr@gmail.com>
2019-05-30 12:27:39 +0200

    First version finish

--- tools/map-security-standard/map_standard_yaml.py
+++ tools/map-security-standard/map_standard_yaml.py
@@ -2,13 +2,52 @@
 # -*- coding: utf8 -*-
 
 import argparse
-import re
 import os
-import glob
 import json
+import glob
 
 
-_sca_file_groups = re.compile(r'compliance:\n\-(.*):(.*)\"\ncondition')
+def get_standards(schema):
+    with open(schema) as f:
+        json_data = json.load(f)
+
+    keys = list()
+    for s in json_data.keys():
+        if s not in keys:
+            splitted = s.split('_')
+            if len(splitted) < 3:
+                if splitted not in keys:
+                    keys.append(splitted[0])
+            else:
+                union = '_'.join(splitted[0:2])
+                if union not in keys:
+                    keys.append(union)
+
+    return keys
+
+
+def add_standard(standard, actual):
+    splitted = actual.strip().split(',')
+    splitted.append(standard)
+    for index, split in enumerate(splitted):
+        splitted[index] = '"'+split+'"'
+    splitted.append('\n')
+    string = ', '.join(splitted)
+    string = list(string)
+    string.pop(-3)
+    return ''.join(string)
+
+
+def get_replacement(standard, schema):
+    for key in standard.keys():
+        version = standard[key]
+        st_key = key
+        to_search = (st_key+'_'+version).strip()
+    if to_search in schema.keys():
+        if schema[to_search] != '' and schema[to_search] not in standard:
+            return schema[to_search]
+
+    return False
 
 
 def pci_to_any(path, schema):
@@ -16,34 +55,35 @@ def pci_to_any(path, schema):
         path += '/'
     with open(schema) as f:
         json_data = json.load(f)
+    list_standards = get_standards(schema)
     os.chdir(path)
-    for file in glob.glob('*.yml'):
+    for file in glob.glob('**/*.yml', recursive=True):
         print('[INFO] Processing {}'.format(file))
-        with open(file, 'r+') as f:
-            lines = f.readlines()
-        new_file = ''
-        for line in lines:
-            match = re.search(_sca_file_groups, line)
-            if match:
-                added = list()
-                standard = json_data[json_data.keys()[0]]
-                standard = ''.join(standard.split('_')[0:2])
-                if standard == match.groups()[0]:
-                    
-
-
-                ##
-                if len(added) > 1:
-                    new_line = line.split(',')
-                    new_line.insert(-1, "".join(added[0:-1]))
-                    new_line = ",".join(new_line)
-                    new_file += new_line
-                else:
+        with open(file) as f:
+            with open(file, 'r+') as f:
+                lines = f.readlines()
+            new_file = ''
+            for line in lines:
+                edited = False
+                for standard in list_standards:
+                    if '- '+standard+':' in line:
+                        splitted_line = line.split('-')
+                        whitespaces = splitted_line[0]
+                        standard = splitted_line[1].strip().split(':')
+                        standard_name = standard[0]
+                        standard[1] = standard[1].replace('"', '').strip()
+                        standard_versions = standard[1]
+                        dict_standard = dict()
+                        dict_standard[standard_name] = standard_versions
+                        replacement = get_replacement(standard=dict_standard, schema=json_data)
+                        if replacement:
+                            new_file += whitespaces+'-'+' '+standard_name+': '+add_standard(replacement, standard_versions)
+                            edited = True
+                        break
+                if not edited:
                     new_file += line
-            else:
-                new_file += line
-        with open(file, 'w') as f:
-            f.write(new_file)
+            with open(file, 'w') as f:
+                f.write(new_file)
 
 
 if __name__ == '__main__':
