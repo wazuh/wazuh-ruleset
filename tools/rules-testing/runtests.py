a8e10f9e6893dafc35734843744f179c70cb149e
Zenidd <jpsaezgutierrez@gmail.com>
2019-05-31 17:19:49 +0200

    Adding erased license info

--- tools/rules-testing/runtests.py
+++ tools/rules-testing/runtests.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 # Copyright (C) 2015-2019, Wazuh Inc.
+#
+# This program is a free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public
+# License (version 2) as published by the FSF - Free Software
+# Foundation
 
 import ConfigParser
 import subprocess
